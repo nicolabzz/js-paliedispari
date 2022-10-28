803afcd79a0e9c0077e3ff4f2cb42c1a4cc86a8f
nicolabzz <nbuzzigoli@gmail.com>
2022-10-28 17:55:12 +0200

    pari-dispari meglio con visualiz. esito in pagina

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,42 +1,32 @@
-// const output = document.getElementById('output');
+const output = document.getElementById('output');
 
-// const numero = parseInt(prompt('Inserisci un numero'));
-
-
-// if (pari(numero))
-// {
-
-// }
+const numero = parseInt(prompt('Inserisci un numero'));
 
 
+if (pari(numero))
+{
+    console.log('pari');
+	output.innerText = 'PARI!'
+}
+else
+{
+	console.log('dispari');
+	output.innerText = 'DISPARI!'
 
-// functon pari(nPari) {
+};
+
+
+function pari(number) {
     
-//     if (nPari % 2)
-//     {
-//         return true;
-//     }
-//     else
-//     {
-//         return false;
-//     }
-    
-// };
-
-
-const userNumber = parseInt(prompt('dammi un numero'));
-
-function isEven(number) {
-	// console.log(number);
-	if (number % 2) { // number != 0
-		return false;
-	} else {
+	console.log(number);
+	
+    if ((number % 2) == 0)
+    {
 		return true;
 	}
-}
-
-if (isEven(userNumber)) {
-	console.log('pari');
-} else {
-	console.log('dispari');
-}
+    else
+    {
+		return false;
+	}
+    
+};
